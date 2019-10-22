023ddc7e3d01018b0432edd48ef10fca8621c67e
saikk9834 <saikk9834@gmail.com>
2019-10-23 01:30:44 +0530

    Update:File sends emails to the concerned person
    The new update to the file can crop the details of the empty shelves and send email to the concerned person regarding the same

--- Test.py
+++ Test.py
@@ -5,6 +5,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 base_dir = './'
 
+#Select the directory where the training and validation data is present
 train_dir = os.path.join(base_dir, 'train')
 validation_dir = os.path.join(base_dir, 'validation')
 
@@ -25,7 +26,7 @@ model.compile(optimizer=RMSprop(lr=0.001), loss='binary_crossentropy', metrics =
 train = ImageDataGenerator( rescale = 1.0/255. )
 test  = ImageDataGenerator( rescale = 1.0/255. )
 
-train_generator = train.flow_from_directory(train_dir, batch_size=20, class_mode='binary', target_size = (150,150))
+train_generator = train.flow_from_directory(train_dir, batch_size=20, class_mode='binary',target_size = (150,150))
 
 validation_generator =  test.flow_from_directory(validation_dir, batch_size=20, class_mode  = 'binary', target_size = (150, 150))
 history = model.fit_generator(train_generator,
@@ -35,3 +36,47 @@ history = model.fit_generator(train_generator,
                               validation_steps=50,
                               verbose=2,
                               use_multiprocessing=False)
+
+#Once the data is trained based on the given dataset, use the model to predict for an image of our choice
+
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+
+tk.Tk().withdraw()
+print("Select the image to be checked")
+file = askopenfilename()
+
+from PIL import Image
+
+img = Image.open(file)
+
+import smtplib
+import os
+import pytesseract
+import cv2
+
+#After the file is selected, try predicting it using the model that we have created. If the image is from the 'empty' directory, or basically it gives '0' as output,
+#Then select the bottom part of the image that has the name of the item and the upper half which contains the details about the image
+#Now apply OCR(Optimal Character Recognition) on the images using pytesseract to extract strings from the cropped images
+if (history.predict(img) == 0):
+    img1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    w,h = img1.size
+    crop_part1 = (0,h/2,w,h)
+    img_1 = img1.crop(crop_part1)
+    filename = "{}.png".format(os.getpid())
+    cv2.imwrite(filename, img_1)
+    text1 = pytesseract.image_to_string(Image.open(filename))
+    os.remove(filename)
+    crop_part2 = (0,h,w,h/2)
+    img_2 = img1.crop(crop_part2)
+    filename = "{}.png".format(os.getpid())
+    cv2.imwrite(filename, img_2)
+    text2 = pytesseract.image_to_string(Image.open(filename))
+    os.remove(filename)
+    content = text1 + text2
+    #Once the data is extracted from the images, create a mail SMTP server to send email to the concerned person containing the concerned data
+    mail = smtplib.SMTP('smtp.gmail.com', 587)
+    mail.starttls() 
+    mail.login("sender_email_id", "sender_email_id_password") 
+    mail.sendmail("sender_email_id", "receiver_email_id", content) 
+    mail.quit() 
